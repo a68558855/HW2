0baf4199d6515fcf81bd5d7d0bdfb71bdf103d00
SCHUMI <a68558855@gmail.com>
2017-10-15 22:23:32 +0800

    HW

--- 3.17/source/main.c
+++ 3.17/source/main.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
+int main()
 {
-	int i, a;
-	float b, c, d, e, f;
-	for (i = 0; i < 1000; i++)
+	int account = 0;
+	float beginning, charge, credits, limit, Blance;
+	while (1)
 	{
-		printf("Enter account number (-1 to end): ");
-		scanf_s("%d", &a);
-		printf("Enter beginning balance: ");
-		scanf_s("%f", &b);
-		printf("Enter total charges: ");
-		scanf_s("%ff", &c);
-		printf("Enter total credits: ");
-		scanf_s("%f", &d);
-		printf("Enter credit limit: ");
-		scanf_s("%f", &e);
-		f = b + c - d;
-		if (f < e)
+		printf("Enter account number(-1 to end):");	scanf_s("%d", &account);
+		if (account != -1)
 		{
-			printf("Accound:\t%d", a);
-			printf("Credict limit:%.2f", e);
-			printf("Balance:\t:%.2f", b + c - d);
-			printf("Credit Limit Exceeded.");
+			printf("Enter beginning balance:");	scanf_s("%f", &beginning);
+			printf("Enter total charge:");		scanf_s("%f", &charge);
+			printf("Enter total credits:");		scanf_s("%f", &credits);
+			printf("Enter credit limit:");		scanf_s("%f", &limit);
+			Blance = beginning + charge - credits;
+			if (Blance > limit)
+			{
+				printf("Account:\t%d\n", account);
+				printf("Credit limit:\t%.2f\n", limit);
+				printf("Balance:\t%.2f\n", Blance);
+				printf("Credit Limit Exceed\n\n");
+			}
+			else { printf("\n"); }
 		}
-		else
-		{
-			system("pause");
-		}
+		else { break; }
+
 	}
-	
+	return 0;
 }
